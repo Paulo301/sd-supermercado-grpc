7f7d77b080e6ddcb4bce1ea5681cbc2443b1de1b
Paulo301 <paulovictor1918@gmail.com>
2022-01-16 18:58:35 -0300

    Atualizacoes

--- client.js
+++ client.js
@@ -22,9 +22,19 @@ const packageDefinition = protoLoader.loadSync(
 
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).supermercado;
 
-const client = new protoDescriptor.PizzaService("127.0.0.1:50051", grpc.credentials.createInsecure());
+const client = new protoDescriptor.SupermercadoService("127.0.0.1:50051", grpc.credentials.createInsecure());
 let isInMenu = true;
 let menuOption = "";
+
+console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
 
 rl.on('line', line => {
   if(isInMenu){
@@ -43,12 +53,21 @@ rl.on('line', line => {
             mensagem += `Id: ${produto.id} \nNome: ${produto.nome} \nPreço: ${produto.preco}\n\n`;
           });
           console.log(mensagem);
+          console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
         }
       });
     } else if(line.trim()==="2"){
       console.log("Digite a id do produto e a quantidade dele, separadas por virgula: Ex: 1, 3");
     } else if(line.trim()==="3"){
-      console.log("Digite a id do produto a ser removido do carrinho");
+      console.log("Digite a id no carrinho do produto a ser removido do carrinho");
     } else if(line.trim()==="4"){
       client.ListarCarrinho({}, (err, result) => {
         if (err) {
@@ -73,10 +92,35 @@ rl.on('line', line => {
             mensagem2 = "Carrinho vazio\n"
           }
           console.log(mensagem2);
+          console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
         }
       });
     } else if(line.trim()==="5"){
-      socket.write("Pagar");
+      client.Pagar({}, (err, result) => {
+        if (err) {
+          console.log("Erro: " + err);
+        } else {
+          const { mensagem } = result;
+          console.log(mensagem);
+          console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
+        }
+      });
     } else if(line.trim()==="6"){
       console.log("Digite a id do pedido para solicitar a entrega");
     } else if(line.trim()==="7"){
@@ -109,6 +153,15 @@ rl.on('line', line => {
             mensagem3 = "Sem pedidos registrados\n"
           }
           console.log(mensagem3);
+          console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
         }
       });
     } else {
@@ -118,52 +171,65 @@ rl.on('line', line => {
     }
   } else {
     if(menuOption==="2"){
-      socket.write("Adicionar_Carrinho,"+line);
+      const params = line.split(",");
+
+      client.AdicionarCarrinho({ "id": params[0], "quantidade": params[1] }, (err, result) => {
+        if (err) {
+          console.log("Erro: " + err);
+        } else {
+          const { mensagem } = result;
+          console.log(mensagem);
+          console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
+        }
+      });
     } else if(menuOption==="3"){
-      socket.write("Remover_Carrinho,"+line);
+      const params = line.split(",");
+
+      client.RemoverCarrinho({ "id": params[0] }, (err, result) => {
+        if (err) {
+          console.log("Erro: " + err);
+        } else {
+          const { mensagem } = result;
+          console.log(mensagem);
+          console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
+        }
+      });
     } else if(menuOption==="6"){
-      socket.write("Solicitar_Entrega,"+line);
+      const params = line.split(",");
+
+      client.SolicitarEntrega({ "id": params[0] }, (err, result) => {
+        if (err) {
+          console.log("Erro: " + err);
+        } else {
+          const { mensagem } = result;
+          console.log(mensagem);
+          console.log("Digite:"+
+                "\n>>>1 -> para listar os produtos"+
+                "\n>>>2 -> para adicionar produtos no carrinho"+
+                "\n>>>3 -> para remover produtos do carrinho"+
+                "\n>>>4 -> para os produtos no carrinho"+
+                "\n>>>5 -> para pagar os produtos no carrinho"+
+                "\n>>>6 -> para solicitar a entrega de um pedido"+
+                "\n>>>7 -> para listar os pedidos"+
+                "\n>>>8 -> para sair");
+        }
+      });
     }
   }
 });
-
-// client.AdicionarPizza({
-//     "id": 1,
-//     "nome": "Calabresa",
-//     "descricao": "Descrição da calabresa",
-//     "preco": 14
-//   }, (err, result) => {
-//     if (err) {
-//         console.log("Erro: " + err);
-//     } else {
-//         console.log("Pizza cadastrada com sucesso!");
-//     }
-//   });
-
-//   client.AdicionarPizza({
-//     "id": 2,
-//     "nome": "Nordestina",
-//     "descricao": "Pizza de carne de sol e manteiga de garrafa",
-//     "preco": 20
-//   }, (err, result) => {
-//     if (err) {
-//         console.log("Erro: " + err);
-//     } else {
-//         console.log("Pizza cadastrada com sucesso!");
-//     }
-//   });
-
-// client.ListarPizzas({}, (err, result) => {
-//   if (err) {
-//       console.log("Erro: " + err);
-//   } else {
-//       console.log("Cardápio:");
-
-//       const { pizzas } = result;
-
-//       for (let i = 0; i < pizzas.length; i++) {
-//           console.log("- Pizza:");
-//           console.log(pizzas[i]);
-//       }
-//   }
-// });
--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const packageDefinition = protoLoader.loadSync(
 
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).supermercado;
 
-// The protoDescriptor object has the full package hierarchy
 const supermercadoService = protoDescriptor.SupermercadoService;
 
 const server = new grpc.Server();
@@ -51,48 +50,78 @@ const bdProdutos = [
 let carrinho = [];
 let pedidos = [];
 
-// implementação do serviço
 server.addService(supermercadoService.service, {
-  // AdicionarPizza: (call, callback) => {
-  //   const pizza = call.request;
-  //   bd.cardapio.push(pizza);
-
-  //   callback(null, {});
-  // },
-
-  // ListarPizzas: (call, callback) => {
-  //     callback(null, { pizzas: bd.cardapio });
-  // },
-
-  // RealizarPedido: (call, callback) => {
-  //   const pedido = call.request;
-  //   const valorTotal = pedido.pizza.preco * pedido.quantidade;
-
-  //   bd.pedidos.push(pedido);
-
-  //   callback(null, { valorTotal });
-  // },
-
   ListarProdutos: (call, callback) => {
     callback(null, { produtos: bdProdutos });
   },
   AdicionarCarrinho: (call, callback) => {
-    const pizza = call.request;
-    bd.cardapio.push(pizza);
-
-    callback(null, {});
+    const dados = call.request;
+    const tempProduto = bdProdutos.find((produto) => produto.id === Number(dados.id));
+    if(tempProduto){
+      const tam = carrinho.length;
+      carrinho.push(
+        {
+          idCarrinho: tam ? carrinho[tam - 1].idCarrinho + 1 : 0,
+          produto: tempProduto,
+          quantidade: Number(dados.quantidade)
+        }
+      );
+      callback(null, { mensagem: "Produto adicionado com sucesso!\n" });
+    } else{
+      callback(null, { mensagem: "Id inválida\n" });
+    }
   },
   RemoverCarrinho: (call, callback) => {
-    callback(null, { pedidos: bd.pedidos });
+    const dados = call.request;
+
+    const tempProduto2 = carrinho.find((produto) => produto.idCarrinho === Number(dados.id));
+    if(tempProduto2){
+      const tempCarrinho = carrinho.filter((produto) => produto.idCarrinho !== Number(dados.id));
+      carrinho = tempCarrinho;
+      callback(null, { mensagem: "Produto removido com sucesso\n" });
+    } else {
+      callback(null, { mensagem: "Id inválida\n" });
+    }
   },
   ListarCarrinho: (call, callback) => {
     callback(null, { itensCarrinho: carrinho });
   },
   Pagar: (call, callback) => {
-    callback(null, { pedidos: bd.pedidos });
+    if(carrinho.length){
+      const tam = pedidos.length;
+      pedidos.push(
+        {
+          idPedido: tam ? pedidos[tam - 1].idPedido + 1 : 0,
+          produtos: carrinho.map((produto) => {
+            const {idCarrinho, ...resto} = produto;
+            return {...resto};
+          }),
+          status: "Pago"
+        }
+      );
+      carrinho = [];
+      callback(null, { mensagem: "Pedido pago com sucesso!\n" });
+    } else {
+      callback(null, { mensagem: "O carrinho está vazio\n" });
+    }
   },
   SolicitarEntrega: (call, callback) => {
-    callback(null, { pedidos: bd.pedidos });
+    const dados = call.request;
+
+    const tempPedido = pedidos.find((pedido) => pedido.idPedido === Number(dados.id));
+    if(tempPedido){
+      const tempPedidos = pedidos.map((pedido) => {
+        if(pedido.idPedido === Number(dados.id)){
+          return {...pedido, status: "Entrega solicitada"}
+        } else{
+          return pedido;
+        }
+      });
+      pedidos = tempPedidos;
+      callback(null, { mensagem: "Entrega solicitada com sucesso\n" });
+    } else {
+      callback(null, { mensagem: "Id inválida\n" });
+    }
   },
   ListarPedidos: (call, callback) => {
     callback(null, { pedidos: pedidos });
